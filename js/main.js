d0cea1bcea2d783462441c39e1ae0eeeb441bca2
Andrea Prina <and.prina@gmail.com>
2022-06-20 18:14:16 +0200

    BONUS 2 - Completato

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const images = [
 
 
 const carouselWrapper = document.getElementById("carousel-wrap");
+const carouselThumbnail = document.getElementById("thumbnails");
 
 // For each image in the list, create an img element and append it to the carousel
 
@@ -19,51 +20,68 @@ for (let i = 0; i < images.length; i++){
     const carouselImage = document.createElement("img");
     carouselImage.setAttribute("src", images[i]);
 
-    // Give the appropriate classes to the element
+    // Create the thumbnail element
+    const thumbElement = document.createElement("div");
+
+    // Give the appropriate classes to the elements
     // If the element is the first one, give it the additional "active" class
     carouselImage.classList.add("ms_carousel-imgs");
+    thumbElement.classList.add("thumb-item");
     if (i === 0){
         carouselImage.classList.add("active");
+        thumbElement.classList.add("active");
     }
 
     // Add the element to the carousel wrapper element
     carouselWrapper.append(carouselImage);
+    carouselThumbnail.append(thumbElement);
 
 }
+
+
+
+
+
 
 // Initialize the index and the buttons
 
 let activeImageIndex = 0;
 const imageList = carouselWrapper.children;
+const thumbList = carouselThumbnail.children;
 const nextButton = document.getElementById("button-next");
 const prevButton = document.getElementById("button-prev");
 
 nextButton.addEventListener("click", function(){
     
-    // Remove the "active" class from the current image
+    // Remove the "active" class from the current image. Do the same to the thumbnail element
     imageList[activeImageIndex].classList.remove("active");
+    thumbList[activeImageIndex].classList.remove("active");
     // Advanced the activeImage counter
     activeImageIndex++;
     // if activeImageIndex > number of images ( >= array length) restart from 0
     if (activeImageIndex === imageList.length){
         activeImageIndex = 0;
     }
-    // Add the "active" class to the following image
+    // Add the "active" class to the following image. Do the same to the thumbnail element
     imageList[activeImageIndex].classList.add("active");
+    thumbList[activeImageIndex].classList.add("active");
 
 })
 
 prevButton.addEventListener("click", function(){
-    // Remove the "active" class from the current image
+    // Remove the "active" class from the current image. Do the same to the thumbnail element
     imageList[activeImageIndex].classList.remove("active");
+    thumbList[activeImageIndex].classList.remove("active");
+
     // Decrement the activeImage counter
     activeImageIndex--;
     // if activeImageIndex < 0 restart from last one
     if (activeImageIndex < 0){
         activeImageIndex = imageList.length - 1;
     }
-    // Add the "active" class to the previous image
+    // Add the "active" class to the previous image. Do the same to the thumbnail element
     imageList[activeImageIndex].classList.add("active");
+    thumbList[activeImageIndex].classList.add("active");
 })
 
 
