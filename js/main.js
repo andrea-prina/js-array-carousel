06cfe2668c536f3a5250257195bb66c265058aab
Andrea Prina <and.prina@gmail.com>
2022-06-20 17:19:27 +0200

    M2 - Inserita singola immagine da lista

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const images = [
+    'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
+    'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
+    'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
+    'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg',
+    'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
+    'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
+    ];
+
+const carouselWrapper = document.getElementById("carousel-wrap");
+
+const carouselImage = document.createElement("img");
+
+carouselImage.setAttribute("src", images[0]);
+carouselImage.classList.add("ms_carousel-imgs");
+
+// console.log(carouselWrapper);
+// console.log(carouselImage);
+
+carouselWrapper.append(carouselImage);
+
